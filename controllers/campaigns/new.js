618232216a304d3fc5a2f140a743aadcfcde3c71
techchida <techchida@outlook.com>
2024-02-19 17:50:17 +0100

    add campaign field

--- controllers/campaigns/new.js
+++ controllers/campaigns/new.js
@@ -6,32 +6,38 @@ const app = express();
 app.post("/", async (req, res) => {
   try {
     let payload = Joi.object({
-      title: Joi.string().required().min(3).max(50).messages({
+      title: Joi.string().required().min(1).max(50).messages({
         "string.empty": "campaign title is required",
         "string.min": "Campaign title requires at least 3 characters",
       }),
-      subtitle: Joi.string().min(10).max(50),
+      campaign: Joi.string().required().min(2).max(50).messages({
+        "string.empty": "campaign title is required",
+        "string.min": "Campaign title requires at least 2 characters",
+      }),
+      subtitle: Joi.string().min(5).max(50),
       type: Joi.string()
         .required()
         .allow("emote", "rating", "generic", "likert")
         .messages({
           "string.allow": "please select a valid campaign type",
         }),
-      name: Joi.boolean(),
-      email: Joi.boolean(),
-      comment: Joi.boolean(),
+      name: Joi.boolean().required(),
+      email: Joi.boolean().required(),
+      comment: Joi.boolean().required(),
     }).validate(req.body);
 
+    console.log(payload);
     if (payload.error)
       return res.status(400).json({
         message: payload.error.details[0].message,
       });
+
     let newCampaign = await CAMPAIGNS.create({
       ...payload.value,
       owner: req.user.id,
     });
 
-    return res.json({
+    return res.status(200).json({
       message: "Campaign created succesfully",
       id: newCampaign._id.toString(),
     });
